2977c29819d65c400dec4c81809f14e8f910e232
Luis Angel López Miranda <miralope.luis@gmail.com>
2025-01-23 17:51:18 -0500

    Sección de Formación completada y agregué el footer para los contactos pero sin estilos

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,3 +3,11 @@ const navLinks = document.getElementById("navLinks");
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 })
+function toggleInfo(button) {
+    const info = button.nextElementSibling;
+    if (info.style.maxHeight) {
+      info.style.maxHeight = null; // Cierra el desplegable
+    } else {
+      info.style.maxHeight = info.scrollHeight + "px"; // Abre el desplegable
+    }
+  }  
